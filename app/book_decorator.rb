522e1fb1aa3da643cb9cf660db45b073b9124a24
Oldrich Vetesnik <oldrich.vetesnik@gmail.com>
2014-03-12 22:34:16 +0100

    Added Bootstrap

--- app/book_decorator.rb
+++ app/book_decorator.rb
@@ -1,8 +1,21 @@
 class BookDecorator < Decorator
-  def percentage_in_parenthesis
+  def rounded_percentage
     percentage = object.percentage
     percentage = percentage.round 1 if percentage.is_a? Float
     percentage = percentage.to_i if percentage % 1 == 0
-    "(#{ percentage } %)"
+    percentage
+  end
+
+  def progress_bar_type
+    case object.percentage.to_i
+    when 0..9
+      'progress-bar-danger'
+    when 10..19
+      'progress-bar-warning'
+    when 20..59
+      'progress-bar-info'
+    else
+      'progress-bar-success'
+    end
   end
 end
